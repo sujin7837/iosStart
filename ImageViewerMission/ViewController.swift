b1ac26b5feca0529427ab88e39e23b665fd0dd0f
sujin7837 <31465594+sujin7837@users.noreply.github.com>
2021-01-11 14:25:58 +0900

    ImageViewer

--- ImageViewerMission/ViewController.swift
+++ ImageViewerMission/ViewController.swift
@@ -8,12 +8,39 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var numImage=1
+    var maxImage=6
+    
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var imageViewer: UILabel!
+    @IBOutlet var btnBefore: UIButton!
+    @IBOutlet var btnAfter: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func btnBeforeImage(_ sender: UIButton) {
+        numImage=numImage-1
+        
+        if(numImage<1) {
+            numImage=maxImage
+        }
+        
+        let imageName=String(numImage) + ".png"
+        imageView.image=UIImage(named: imageName)
+    }
+    
+    @IBAction func btnAfterImage(_ sender: UIButton) {
+        numImage=numImage+1
+        
+        if(numImage>maxImage) {
+            numImage=1
+        }
+        
+        let imageName=String(numImage) + ".png"
+        imageView.image=UIImage(named: imageName)
+    }
 }
 
